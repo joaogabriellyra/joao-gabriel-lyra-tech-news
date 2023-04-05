c617c1879b6e638bd7e117a2cf99f7fa5884adaa
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-04-05 03:11:29 -0300

    requisito 10 concluído

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,15 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+
+    categories = []
+    for new in news:
+        categories.append(new["category"])
+    filtered_categories = []
+    for category in Counter(sorted(categories)).most_common()[0:5]:
+        filtered_categories.append(category[0])
+    return filtered_categories
--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,12 @@
+output = ("""Selecione uma das opções a seguir:
+ 0 - Popular o banco com notícias;
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por categoria;
+ 4 - Listar top 5 categorias;
+ 5 - Sair.""")
+
+
 # Requisitos 11 e 12
 def analyzer_menu():
     """Seu código deve vir aqui"""
