e78a5c7fcd228a37435ea27180c4e008f637f9d0
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-04-05 02:45:01 -0300

    requisito 7 concluído

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    filtered_news_output = []
+    filtered_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    for new in filtered_news:
+        filtered_news_output.append((new["title"], new["url"]))
+    return filtered_news_output
 
 
 # Requisito 8
