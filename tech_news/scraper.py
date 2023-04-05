c081633464bdc112db414f05e19f030f57b38c7b
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-04-04 23:59:27 -0300

    requisitos 2, 3 e 4 concluídos

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,7 @@
 import requests
 from time import sleep
+from parsel import Selector
+from bs4 import BeautifulSoup
 
 
 # Requisito 1
@@ -17,17 +19,35 @@ def fetch(url):
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    links = selector.css("header.entry-header h2 a::attr(href)").getall()
+    return links
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next = selector.css("a.next::attr(href)").get()
+    return next
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    soup = BeautifulSoup(html_content, "html.parser")
+    selector = Selector(html_content)
+    one_new = {}
+    one_new["url"] = soup.find("link", rel="canonical").get("href")
+    one_new["title"] = selector.css("div.entry-header-inner h1::text").get(
+
+    ).strip()
+    one_new["timestamp"] = selector.css("li.meta-date::text").get()
+    one_new["writer"] = selector.css("span.author a::text").get()
+    one_new["reading_time"] = int(selector.css(
+        "li.meta-reading-time::text").re_first(r"\d+"))
+    one_new["summary"] = soup.find(
+        "div", {"class": "entry-content"}).p.text.strip()
+    one_new["category"] = selector.css("span.label::text").get()
+    return one_new
 
 
 # Requisito 5
